eb3c16a11c0b8f556cf183566442948991da341b
SebaGut2103 <juansefernando.guti2103@gmail.com>
2025-05-29 14:48:24 -0500

    Creando Proyecto

---
+++ EjercicioSpring/src/test/java/EjercicioSpringBoot/demo/ApplicationTests.java
@@ -0,0 +1,13 @@
+package EjercicioSpringBoot.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
